92866a4aeb2f252c2000f893d0145aa80e265d89
itsmesachitsaran <itsmesachitsaran2@gmail.com>
2026-01-07 12:43:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import yfinance as yf
 
 app = Flask(__name__)
 
@@ -8,11 +9,34 @@ def home():
 
 @app.route("/predict/<stock>")
 def predict(stock):
-    return jsonify({
-        "stock": stock.upper(),
-        "trend": "Bullish",
-        "confidence": "60%"
-    })
+    try:
+        data = yf.download(
+            stock + ".NS",
+            period="6mo",
+            progress=False
+        )
+
+        if data.empty:
+            return jsonify({"error": "Invalid NSE stock"})
+
+        close = float(data["Close"].iloc[-1])
+        ma20 = float(data["Close"].rolling(20).mean().iloc[-1])
+
+        if close > ma20:
+            trend = "Bullish"
+            confidence = "65%"
+        else:
+            trend = "Bearish"
+            confidence = "60%"
+
+        return jsonify({
+            "stock": stock.upper(),
+            "trend": trend,
+            "confidence": confidence
+        })
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
